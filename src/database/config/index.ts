46e9c33d1971760a854d9daa4292733672dc7d86
fiaz <mlkfiazh1@gmail.com>
2026-08-03 19:34:27 +0500

    add srv config issue

--- src/database/config/index.ts
+++ src/database/config/index.ts
@@ -2,9 +2,11 @@ import {
   MongooseModuleOptions,
   MongooseOptionsFactory,
 } from '@nestjs/mongoose';
+import { setServers } from 'dns';
 
 export class MongooseConfig implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
+    setServers(['8.8.8.8', '1.1.1.1']);
     return {
       uri: process.env.MONGODB_URL,
       dbName: process.env.MONGODB_DATABASE_NAME,
